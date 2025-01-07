5756f0d75dd7ee6c554266a8df784014149c5067
hnmike <nnamno2612@gmail.com>
2025-01-07 22:23:40 +0700

    Push changes to master

--- Web_CuaHangCafe/obj/Debug/net8.0/Web_CuaHangCafe.AssemblyInfo.cs
+++ Web_CuaHangCafe/obj/Debug/net8.0/Web_CuaHangCafe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_CuaHangCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81bd8b39c83d0bf860f15741696f10d6f932159d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f801c8f11c81db39efff442d29d0f31703c0e36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_CuaHangCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_CuaHangCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Web_CuaHangCafe/push_to_git.bat
@@ -0,0 +1,4 @@
+@echo off
+git add .
+git commit -m "Push changes to master"
+git push https://github.com/hnmike/aspnet.git master
